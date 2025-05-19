c60a768792d2d9e73252b36b439ac130af1cb0b1
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-05-18 21:10:41 -0500

    chore: add buttonColors object for button styling in styledcomponents.ts

--- src/theme/styledcomponents.ts
+++ src/theme/styledcomponents.ts
@@ -5,6 +5,19 @@ import styled from 'styled-components';
 type ButtonVariant = 'dark' | 'light';
 type ButtonSize = 'small' | 'medium' | 'large';
 
+const buttonColors = {
+  dark: {
+    bg: 'rgba(26, 25, 30, 1)',
+    hover: 'rgba(51, 51, 51, 1)',
+    color: '#FFFFFF',
+  },
+  light: {
+    bg: 'rgba(243, 243, 243, 1)',
+    hover: 'rgba(221, 221, 221, 1)',
+    color: '#000000',
+  },
+};
+
 //Text
 //Inputs
 //Cards
