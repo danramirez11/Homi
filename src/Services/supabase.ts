9b3e7499e091d73f9ec10448d58a809bf1dd6ab1
Dan <daramirezp1112@gmail.com>
2025-05-20 12:03:00 -0500

    fixing .env

--- src/Services/supabase.ts
+++ src/Services/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_PROYECT_SUPABASEKEY
+const VITE_PROYECT_SUPABASEKEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodGduZmVjcmliZWpzeHdjcXF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNzM4OTMsImV4cCI6MjA2Mjg0OTg5M30.5U1mV2V-2sypKYuWdAOzd0pE0Gc-08Cv_uUyVXmEzdg'
+const VITE_SUPABASE_URL='https://ahtgnfecribejsxwcqqv.supabase.co/'
+
+const supabaseUrl = VITE_SUPABASE_URL
+const supabaseKey = VITE_PROYECT_SUPABASEKEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
