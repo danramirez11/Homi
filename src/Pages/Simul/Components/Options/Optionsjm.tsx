fce4bd233f7a48c179b727809bcbca6baae11aae
Soleil <solangegarciaguamanga@gmail.com>
2025-05-20 11:27:58 -0500

    Style Component Simulation

--- src/Pages/Simul/Components/Options/Optionsjm.tsx
+++ src/Pages/Simul/Components/Options/Optionsjm.tsx
@@ -1,10 +1,29 @@
 import './Optionscss.css'
+import { Text } from '../../../../theme/styledcomponents'
 
 const Options =() =>{
     return(
         <div>
             <div className="circlevector">
             <img src="src\assets\hand-coins.svg" alt="" />
+                            <div>
+                    <div>
+                        <div>
+                            <p>Aplica al subsidio de vivienda (Mi Casa Ya u otros)</p>
+                        </div>
+                        <div>
+                            <p>Elige un proyecto con una cuota inicial más baja</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Usa más recursos como cesantías, primas o ingresos familiares</p>
+                        </div>
+                        <div>
+                            <p>Agenda una cita con un asesor de Jaramillo Mora</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Pages/Simul/Simul.tsx
+++ src/Pages/Simul/Simul.tsx
@@ -54,24 +54,6 @@ const Simul = ()=> {
                 <div>
                 <p>¿Deseas conocer otras opciones?</p>
                 <Options></Options>
-                <div>
-                    <div>
-                        <div>
-                            <p>Aplica al subsidio de vivienda (Mi Casa Ya u otros)</p>
-                        </div>
-                        <div>
-                            <p>Elige un proyecto con una cuota inicial más baja</p>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <p>Usa más recursos como cesantías, primas o ingresos familiares</p>
-                        </div>
-                        <div>
-                            <p>Agenda una cita con un asesor de Jaramillo Mora</p>
-                        </div>
-                    </div>
-                </div>
             </div>
 
         </section>
