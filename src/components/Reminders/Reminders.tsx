0e809f4bec439e9170f1ecc0a2c19fecea669e94
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-06-02 19:27:44 -0500

    refactor: update text variants for consistency in Reminders component

--- src/components/Reminders/Reminders.tsx
+++ src/components/Reminders/Reminders.tsx
@@ -13,20 +13,20 @@ const Reminders: React.FC = () => {
     <BsCurrencyDollar size={22} />
   </div>
         <div className="text">
-          <Text variant="bodyBold" color="#2C2B2B">Tu próximo pago es:</Text>
-          <Text variant="captionRegular" color="#888">• En 24 días</Text>
+          <Text variant="captionBold" color="#2C2B2B">Tu próximo pago es:</Text>
+          <Text variant="captionRegular" color="#B1B1B1">• En 24 días</Text>
         </div>
       </div>
 
-      <Text variant="sectionTitle" color="#2C2C2C">Alertas</Text>
+      <Text variant="subtitle" color="#2C2C2C">Alertas</Text>
 
       <div className="reminder-box red">
   <div className="reminder-icon">
     <BsBell size={22} />
   </div>
         <div className="text">
-          <Text variant="bodyBold" color="#2C2C2C">Tu pago esta atrasado</Text>
-          <Text variant="captionRegular" color="#888">• Llevas 4 días en Mora</Text>
+          <Text variant="captionBold" color="#2C2C2C">Tu pago esta atrasado</Text>
+          <Text variant="captionRegular" color="#B1B1B1">• Llevas 4 días en Mora</Text>
         </div>
       </div>
     </div>
