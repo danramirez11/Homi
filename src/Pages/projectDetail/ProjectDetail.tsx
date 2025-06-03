ad522ae9b6f719795ad02514be4cdb70512c7efa
Dan <daramirezp1112@gmail.com>
2025-06-02 22:18:56 -0500

    feat(dash): add project details components

--- src/Pages/projectDetail/ProjectDetail.tsx
+++ src/Pages/projectDetail/ProjectDetail.tsx
@@ -6,6 +6,8 @@ import { Text } from "../../theme/styledcomponents";
 import './ProjectDetail.css'
 import StateGraph from "../../components/StateGraph/StateGraph";
 import ProNews from "../../components/ProNews/ProNews";
+import { PieChart } from '@mui/x-charts/PieChart';
+import Reminders from "../../components/Reminders/Reminders";
 
 const ProjectDetail = () => {
 
@@ -31,7 +33,38 @@ const ProjectDetail = () => {
             </section>
 
             <section className="project-right">
-                <h1>hi</h1>
+                <section className="project-content-card">
+                    <Text variant="cardSubtitle">Mis pagos (estado financiero)</Text>
+                    <PieChart
+                        series={[
+                            {
+                            data: [
+                                { id: 1, value: 32, label: 'Pagado 32%', color: '#92BFFF' },
+                                { id: 0, value: 68, label: 'Deuda 68%', color: 'gray' }
+                            ],
+                            innerRadius: 50,
+                            paddingAngle: 5,
+                            cornerRadius: 5,
+                            startAngle: 0,
+                            endAngle: 360,
+                            },
+                        ]}
+                         slotProps={{
+                            legend: {
+                            direction: 'horizontal',
+                            position: {
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            },
+                            },
+                        }}
+                        width={170}
+                        height={170}
+                    />
+                </section>
+                <section className="project-content-card">
+                    <Reminders/>
+                </section>
             </section>
         </div>
       </div>
