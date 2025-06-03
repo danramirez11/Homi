cb359f1f98d768d4a4ec6c5618e2cd597cce2629
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-06-02 19:41:10 -0500

    refactor: remove unused Calendar component and related state management in CalendarWidget

--- src/components/CalendarWidget/CalendarWidget.tsx
+++ src/components/CalendarWidget/CalendarWidget.tsx
@@ -8,23 +8,7 @@ const CalendarWidget: React.FC = () => {
 
   return (
     <div className="calendar-widget">
-      <Calendar
-        onChange={(val) => {
-          if (val instanceof Date) {
-            setValue(val);
-          } else if (Array.isArray(val) && val[0] instanceof Date) {
-            setValue(val[0]);
-          }
-        }}
-        value={value}
-        calendarType="iso8601"
-        next2Label={null}
-        prev2Label={null}
-        locale="es-ES"
-        formatShortWeekday={(locale, date) =>
-          date.toLocaleDateString(locale, { weekday: 'short' }).substring(0, 2)
-        }
-      />
+      
     </div>
   );
 };
