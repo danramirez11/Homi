9e9adc22f0b5ff75583001b9f579726d5c47642b
Sofia Sarasty <sofia.sarasty.12@gmail.com>
2025-06-04 11:49:06 -0500

    feat(dash): update project card images and details for improved presentation

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -20,8 +20,8 @@ const Dashboard: React.FC = () => {
             <Text variant="sectionTitle">Mis Proyectos</Text>
 
             <ProjectCard
-              backgroundImage="/assets/origen.webp"
-              logo="/assets/origen-logo.png"
+              backgroundImage="https://www.jaramillomora.com/wp-content/uploads/2025/01/origen-galeria-9-scaled.webp"
+              logo="https://ahtgnfecribejsxwcqqv.supabase.co/storage/v1/object/sign/proyectos/CompactoF(logo).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2Q2NzhmZDkzLTg1ZjgtNDEzOS1hMTcyLTFiN2ZiNjQ3ZTYzNyJ9.eyJ1cmwiOiJwcm95ZWN0b3MvQ29tcGFjdG9GKGxvZ28pLnBuZyIsImlhdCI6MTc0ODcwMzk4MywiZXhwIjoxNzgwMjM5OTgzfQ.pbu-0RTCyYL5zM8ed8lSJ9gsWdPNEIq_RFO53RzNyqY"
               location="Pance"
               stage="Etapa III"
               name="Apartamentos Campestres"
@@ -30,20 +30,20 @@ const Dashboard: React.FC = () => {
             />
 
             <ProjectCard
-              backgroundImage="/assets/guabinas.webp"
-              logo="/assets/mongui-logo.png"
+              backgroundImage="https://storage.googleapis.com/bucket-jm-web/wp-content/uploads/2024/08/zona-picinic-tamarindo.webp"
+              logo="https://ahtgnfecribejsxwcqqv.supabase.co/storage/v1/object/sign/proyectos/CompactoF(logo).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2Q2NzhmZDkzLTg1ZjgtNDEzOS1hMTcyLTFiN2ZiNjQ3ZTYzNyJ9.eyJ1cmwiOiJwcm95ZWN0b3MvQ29tcGFjdG9GKGxvZ28pLnBuZyIsImlhdCI6MTc0ODcwMzk4MywiZXhwIjoxNzgwMjM5OTgzfQ.pbu-0RTCyYL5zM8ed8lSJ9gsWdPNEIq_RFO53RzNyqY"
               location="Yumbo"
               stage="Etapa I"
               name="Ciudad Guabinas"
               tower="Torre 1 - 204"
             />
             <ProjectCard
-              backgroundImage="/assets/guabinas.webp"
-              logo="/assets/mongui-logo.png"
-              location="Yumbo"
-              stage="Etapa I"
-              name="Ciudad Guabinas"
-              tower="Torre 1 - 204"
+              backgroundImage="https://storage.googleapis.com/bucket-jm-web/wp-content/uploads/2024/10/galeria-titanio-piscina.webp"
+              logo="https://ahtgnfecribejsxwcqqv.supabase.co/storage/v1/object/sign/proyectos/CompactoF(logo).png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5X2Q2NzhmZDkzLTg1ZjgtNDEzOS1hMTcyLTFiN2ZiNjQ3ZTYzNyJ9.eyJ1cmwiOiJwcm95ZWN0b3MvQ29tcGFjdG9GKGxvZ28pLnBuZyIsImlhdCI6MTc0ODcwMzk4MywiZXhwIjoxNzgwMjM5OTgzfQ.pbu-0RTCyYL5zM8ed8lSJ9gsWdPNEIq_RFO53RzNyqY"
+              location="Sur de Cali"
+              stage="Etapa II"
+              name="Titanio Apartamentos"
+              tower="Torre 2 - 504"
             />
           </div>
 
