ad1ae8cd0cb24d7e848f9c81828f79c1fcd466a2
Dan <daramirezp1112@gmail.com>
2025-06-04 11:57:18 -0500

    refactor: update background images and styles for ProBanner and ProNews components

--- src/components/ProBanner/ProBanner.tsx
+++ src/components/ProBanner/ProBanner.tsx
@@ -4,7 +4,7 @@ import './ProBanner.css'
 const ProBanner = () => {
     return (
         <div className="pro-banner">
-        <img src="https://ahtgnfecribejsxwcqqv.supabase.co/storage/v1/object/sign/proyectos/PremiumF(logo).webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5XzAwNzA2YzcwLWMzOTktNDUwZi04MWM4LWJhYzVmNjhhN2NhNCJ9.eyJ1cmwiOiJwcm95ZWN0b3MvUHJlbWl1bUYobG9nbykud2VicCIsImlhdCI6MTc0Nzc1OTczOCwiZXhwIjoxNzc5Mjk1NzM4fQ.BaGpYWE1HyVGswLU9xQVQXAaGiWXcp-zPIXxeqC32M4" alt="" />
+        <img src="https://ahtgnfecribejsxwcqqv.supabase.co/storage/v1/object/public/proyectos//PremiumF(logo).webp" alt="" />
         <Text variant='bodyRegular' className="inline" color='white'>Etapa III</Text>
         </div>
     );
