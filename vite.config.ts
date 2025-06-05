3e8fb3566dcd3c639b89f85541e8f8d61f056b4c
Dan <daramirezp1112@gmail.com>
2025-06-05 14:43:36 -0500

    refactor: remove babel plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import  babel  from 'vite-plugin-babel'
 
 export default defineConfig({
   plugins: [
     react(),
-    babel(), // usa tu babel.config.js
   ],
 })
